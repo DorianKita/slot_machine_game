2a0ffc0a81540c8336a53fc533014a36f3464cc8
Dorian Kita <dorian.kita90@gmail.com>
2023-07-12 21:50:27 +0200

    created method to add game statistics

--- Statistics.js
+++ Statistics.js
@@ -1,5 +1,14 @@
 class Statistics {
     constructor() {
-
+        this.gameResults = [];
     };
+
+    addGameToStatistics(win, bid) {
+        let gameResult = { win, bid }
+        this.gameResults.push(gameResult)
+        console.log(gameResult)
+    }
+
 };
+
+const stat = new Statistics();
