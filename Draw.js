b18929257d7a916f07e7d851a3edc797ae4a389d
Dorian Kita <dorian.kita90@gmail.com>
2023-07-12 22:44:43 +0200

    added drawResult method for drawing colors

---
+++ Draw.js
@@ -0,0 +1,21 @@
+class Draw {
+    constructor() {
+        this.options = ["red", "green", "blue"];
+        let _result = this.drawResult();
+
+        //access to _result
+        this.getResult = () => _result
+    };
+
+    //
+    drawResult() {
+        let colors = [];
+        this.options.forEach(option => {
+            const index = Math.floor(Math.random(option) * this.options.length)
+            colors.push(this.options[index])
+        })
+        return colors
+    };
+};
+
+const draw = new Draw();
