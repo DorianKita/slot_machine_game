289b1f3e101ad6aa721e70ec23273f70ff78140b
Dorian Kita <dorian.kita90@gmail.com>
2023-07-12 22:08:45 +0200

    added ShowGameStatistics method for displaying game statistics

--- Statistics.js
+++ Statistics.js
@@ -3,12 +3,20 @@ class Statistics {
         this.gameResults = [];
     };
 
+    //method for adding game statistics
     addGameToStatistics(win, bid) {
         let gameResult = { win, bid }
         this.gameResults.push(gameResult)
         console.log(gameResult)
     }
 
+    //method for displaying game statistics
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(game => game.win).length
+        let losses = this.gameResults.filter(game => !game.win).length
+        return [games, wins, losses];
+    }
 };
 
 const stat = new Statistics();
