8857847c6648577cb17870982b94d950eac48aea
Dorian Kita <dorian.kita90@gmail.com>
2023-07-13 19:56:22 +0200

    added Result class with MoneyWinInGame method

---
+++ Result.js
@@ -0,0 +1,15 @@
+class Result {
+    static moneyWinInGame(result, bid) {
+        if (result) return bid * 3
+        else return 0
+    };
+
+    static checkIfWin(drawResult) {
+        if (drawResult[0] === drawResult[1] && drawResult[1] === drawResult[2] || drawResult[0] !== drawResult[1] && drawResult[1] !== drawResult[2] && drawResult[0] !== drawResult[2]) return true;
+        return false;
+    }
+};
+
+Result.moneyWinInGame(false, 10);
+
+console.log(Result.checkIfWin(["red", "green", "blue"]))
--- Statistics.js
+++ Statistics.js
@@ -19,4 +19,4 @@ class Statistics {
     }
 };
 
-const stat = new Statistics();
+// const stat = new Statistics();
